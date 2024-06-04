19c7868653986ebb5ff602c5b0af7dfb1b888624
Mohamed <muhammad.elgallai@gmail.com>
2024-06-04 13:02:15 -0500

    creating initUI method

--- GUI.py
+++ GUI.py
@@ -1,13 +1,8 @@
 import sys
-import os
-import warnings
-
-# Ignore specific deprecation warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QListJsonWidget, QLabel, QLineEdit, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog, QListWidget, QLabel, QLineEdit, QMessageBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
+import os
 
 # Define the main class for the Photo Organizer application, inheriting from QMainWindow
 class PhotoOrganizer(QMainWindow):
@@ -18,3 +13,25 @@ class PhotoOrganizer(QMainWindow):
         self.initUI()
         # Initialize a dictionary to store album data
         self.albums = {}
+
+
+    def initUI(self):
+            # Set the window title
+            self.setWindowTitle('Photo Organizer')
+            # Set the window geometry (x, y, width, height)
+            self.setGeometry(100, 100, 800, 600)
+
+            # Create the main widget
+            self.mainWidget = QWidget()
+
+
+# Main entry point of the application
+if __name__ == '__main__':
+    # Create an application instance
+    app = QApplication(sys.argv)
+    # Create an instance of the PhotoOrganizer class
+    ex = PhotoOrganizer()
+    # Show the main window
+    ex.show()
+    # Execute the application event loop
+    sys.exit(app.exec_())
