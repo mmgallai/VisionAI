ee7a7c8444671b102ccd58f50aacb034381a99cf
Jagruth Reddy <56351734+jagruthreddy@users.noreply.github.com>
2024-06-04 21:30:33 -0500

    Update model.py
    Initial Code

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt") 
+
+# Use the model
+model.train(data="coco128.yaml", epochs=3)  
+metrics = model.val()  
+results = model("https://ultralytics.com/images/bus.jpg")  
+path = model.export(format="onnx") 
 
