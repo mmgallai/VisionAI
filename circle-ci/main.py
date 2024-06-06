0d0feeee7b6d1c233eb9bdc261f58640492efb50
Mohamed <muhammad.elgallai@gmail.com>
2024-06-06 12:43:52 -0500

    test folder for circle-ci

---
+++ circle-ci/main.py
@@ -0,0 +1,9 @@
+def Add(a,b):
+    return a + b
+
+def SayHello():
+    print("Hello team!!")
+
+
+if __name__ == "__main__":
+    SayHello()
---
+++ circle-ci/test-main.py
@@ -0,0 +1,8 @@
+from main import Add
+
+def TestAdd():
+    assert Add(2,3) == 5
+    print("Add funtion works currectly")
+
+if __name__ == "__main__":
+    TestAdd()
