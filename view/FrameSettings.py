26e61732b48fd231b30a21941730a9aa5bb6fce6
Mohamed <muhammad.elgallai@gmail.com>
2024-06-06 02:16:36 -0500

    adding view folder

---
+++ view/FrameSettings.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
+
+class FrameSettings:
+    def __init__(self, widget, color=Qt.black):
+        self.widget = widget
+        self.color = color
+        self.setup_frame()
+
+    def setup_frame(self):
+        # Set the background color
+        palette = self.widget.palette()
+        palette.setColor(QPalette.Background, QColor(self.color))
+        self.widget.setAutoFillBackground(True)
+        self.widget.setPalette(palette)
+
+        # Create a grid layout
+        self.layout = QGridLayout()
+        self.widget.setLayout(self.layout)
---
+++ view/MainView.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QStyle
+from PyQt5.QtCore import Qt
+from view.FrameSettings import FrameSettings
+from view.IconButton import IconButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Main Frame")
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.frame_settings = FrameSettings(self.central_widget)
+        self.create_icon_button()
+        self.showMaximized()
+
+    def create_icon_button(self):
+        label_text = "Name of the album\nNumber of photos"
+        button_size = (128, 128)
+        icon_size = (100, 100)
+        button = IconButton(QStyle.SP_DirIcon, label_text, button_size, icon_size, self.central_widget)
+        self.frame_settings.layout.addWidget(button, 0, 0, alignment=Qt.AlignTop | Qt.AlignLeft)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
