5514efef38a69bcde41f965d9a4103cf57968e5f
Mohamed <muhammad.elgallai@gmail.com>
2024-06-09 18:05:15 -0500

    Changing class name from PopUpWindow to SelectMethod

---
+++ view/SelectMethod.py
@@ -0,0 +1,71 @@
+from PyQt5.QtWidgets import QFileDialog, QPushButton, QVBoxLayout, QLabel, QDialog
+from PyQt5.QtCore import Qt
+from controller.AI import AI
+from controller.Manual import Manual
+
+class SelectMethod(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Choose Classification Method")
+        self.setStyleSheet("background-color: #2b2b2b; color: white;")
+        self.setFixedSize(500, 250)
+
+        self.layout = QVBoxLayout(self)
+
+        self.label = QLabel("Do you want to classify the photos using Vision AI or manually?", self)
+        self.label.setStyleSheet("color: white; font-size: 18px;")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setWordWrap(True)
+        self.layout.addWidget(self.label)
+
+        self.vision_ai_button = QPushButton("Vision AI", self)
+        self.vision_ai_button.setStyleSheet(
+            """
+            QPushButton {
+                border: 2px solid #3EB489; 
+                color: white;
+                font-size: 18px;
+                border-radius: 15px;
+                padding: 10px 20px;
+                background-color: transparent;
+            }
+            QPushButton:hover {
+                background-color: #3EB489;
+            }
+            """
+        )
+        self.layout.addWidget(self.vision_ai_button)
+
+        self.manual_button = QPushButton("Manually", self)
+        self.manual_button.setStyleSheet(
+            """
+            QPushButton {
+                border: 2px solid #3EB489; 
+                color: white;
+                font-size: 18px;
+                border-radius: 15px;
+                padding: 10px 20px;
+                background-color: transparent;
+            }
+            QPushButton:hover {
+                background-color: #3EB489;
+            }
+            """
+        )
+        self.layout.addWidget(self.manual_button)
+
+        self.vision_ai_button.clicked.connect(self.use_vision_ai)
+        self.manual_button.clicked.connect(self.use_manual)
+
+    def use_vision_ai(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
+        if folder_path:
+            ai_instance = AI()
+            ai_instance.classify_folder(folder_path)
+            self.parent().update_view(folder_path)
+        self.accept()
+
+    def use_manual(self):
+        manual_instance = Manual(self.parent(), self.parent().initial_directory)
+        manual_instance.classify_images()
+        self.accept()
