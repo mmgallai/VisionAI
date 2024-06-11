fcad6b6a0cc80d334d10f22d86d08489794e801d
jagruthh <jagruthreddy9999@gmail.com>
2024-06-11 00:00:52 -0500

    Best Image View

--- view/ImageDisplay.py
+++ view/ImageDisplay.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QScrollArea, QWidget, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QScrollArea, QWidget, QGridLayout, QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import os
@@ -12,27 +12,33 @@ class ImageDisplay(QScrollArea):
     def display_single_image(self, image_path):
         self.clear_layout()
         scroll_widget = QWidget()
-        scroll_layout = QHBoxLayout(scroll_widget)  # Changed to QHBoxLayout
-        pixmap = QPixmap(image_path).scaled(200, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        scroll_layout = QGridLayout(scroll_widget)
+        pixmap = QPixmap(image_path).scaled(400, 400, Qt.KeepAspectRatio, Qt.SmoothTransformation)
         label = QLabel()
         label.setPixmap(pixmap)
         label.setAlignment(Qt.AlignCenter)
-        scroll_layout.addWidget(label)
+        scroll_layout.addWidget(label, 0, 0, 1, 4)  # Single image takes the entire row
         self.setWidget(scroll_widget)
         self.parent.frame_settings.layout.addWidget(self, 2, 0)
 
     def display_folder_contents(self, folder_path):
         self.clear_layout()
         scroll_widget = QWidget()
-        scroll_layout = QHBoxLayout(scroll_widget)  # Changed to QHBoxLayout
+        scroll_layout = QGridLayout(scroll_widget)
         image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))]
+        row = 0
+        col = 0
         for image_file in image_files:
             image_path = os.path.join(folder_path, image_file)
-            pixmap = QPixmap(image_path).scaled(200, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            pixmap = QPixmap(image_path).scaled(400, 400, Qt.KeepAspectRatio, Qt.SmoothTransformation)
             label = QLabel()
             label.setPixmap(pixmap)
             label.setAlignment(Qt.AlignCenter)
-            scroll_layout.addWidget(label)
+            scroll_layout.addWidget(label, row, col)
+            col += 1
+            if col == 4:  # Move to the next row after 3 images
+                col = 0
+                row += 1
         self.setWidget(scroll_widget)
         self.parent.frame_settings.layout.addWidget(self, 2, 0)
         self.parent.update_image_count_label(folder_path)
