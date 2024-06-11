5d87644af2fe21161728bc07d05095edf47786eb
jagruthh <jagruthreddy9999@gmail.com>
2024-06-10 23:49:00 -0500

    Horizontal image view feature

--- view/ImageDisplay.py
+++ view/ImageDisplay.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QScrollArea, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QScrollArea, QWidget, QHBoxLayout, QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 import os
@@ -12,7 +12,7 @@ class ImageDisplay(QScrollArea):
     def display_single_image(self, image_path):
         self.clear_layout()
         scroll_widget = QWidget()
-        scroll_layout = QVBoxLayout(scroll_widget)
+        scroll_layout = QHBoxLayout(scroll_widget)  # Changed to QHBoxLayout
         pixmap = QPixmap(image_path).scaled(200, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
         label = QLabel()
         label.setPixmap(pixmap)
@@ -24,7 +24,7 @@ class ImageDisplay(QScrollArea):
     def display_folder_contents(self, folder_path):
         self.clear_layout()
         scroll_widget = QWidget()
-        scroll_layout = QVBoxLayout(scroll_widget)
+        scroll_layout = QHBoxLayout(scroll_widget)  # Changed to QHBoxLayout
         image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))]
         for image_file in image_files:
             image_path = os.path.join(folder_path, image_file)
